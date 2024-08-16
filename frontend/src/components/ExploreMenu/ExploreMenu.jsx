3d7cae9bd7477b0f87f25c0050d447f818b9edc4
Priyanshu Chaniyara <piyuthehero@gmail.com>
2024-08-16 15:51:52 +0530

    Add new components and styles for Footer, Cart, PlaceOrder, Header, and App

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets'
+const ExploreMenu = ({category,setCategory}) => {
+  return (
+    <div className='explore-menu' id='explore-menu'>
+        <h1>Explore our menu</h1>
+        <p className='explore-menu-text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos nesciunt obcaecati, omnis, autem ab quos velit, similique a ea doloribus cupiditate non? Earum, mollitia velit.</p>
+      <div className="explore-menu-list">
+        {menu_list.map((item,index)=>{
+            return(
+                <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className='explore-menu-list-item'>
+                    <img className={category=== item.menu_name?"active":""} src={item.menu_image} alt="" />
+                    <p>{item.menu_name}</p>
+                </div>
+            )
+        })}
+      </div>
+      <hr />
+    </div>
+  )
+}
+
+export default ExploreMenu
