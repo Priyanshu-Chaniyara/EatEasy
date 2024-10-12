5858a72bdb7ced1fb8153d8a7839cab0d7d6c1c2
Priyanshu Chaniyara <piyuthehero@gmail.com>
2024-10-12 14:28:03 +0530

    Complete User page and initiated admin page
    feat: Add new components and styles for List, Orders, Navbar, and Sidebar

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -24,4 +24,29 @@ const addFood = async (req, res) => {
   }
 };
 
-export { addFood };
+//all food list
+const listFood = async (req, res) => {
+    try {
+      const foods = await foodModel.find({});
+      req.json({success:true,data:foods});
+    } catch (error) {
+      console.log(error);
+      req.json({success:false,message:"Error"});
+    }
+};
+
+//remove food item
+const removeFood = async (req,res) => {
+try {
+  const food = await foodModel.findById(req.body.id);
+  fs.unlinkSync(`uploads/${food.image}`,()=>{});
+
+  await foodModel.findByIdAndDelete(req.body.id);
+  res.json({success:true,message:"Food Removed"});
+} catch (error) {
+  console.log(error);
+  res.json({success:false,message:"Error"});
+}
+}
+
+export { addFood , listFood , removeFood };
